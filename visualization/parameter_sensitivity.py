45fe5adbe5fe18163b776e4f8f0faacda0272b2f
Tidiane <camaret.tidiane@mail.com>
2022-11-28 21:14:47 +0100

    wandb logging

--- visualization/parameter_sensitivity.py
+++ visualization/parameter_sensitivity.py
@@ -6,6 +6,8 @@ from visualization.train_and_eval_agent import train_and_eval_agent, agent_stati
 from omegaconf import OmegaConf
 import numpy as np
 import matplotlib.pyplot as plt
+import wandb
+
 
 config = OmegaConf.load('config.yaml')
 num_agent_runs = config.model.num_agent_runs
@@ -15,50 +17,76 @@ agent_training_timesteps = num_agent_runs * model_training_steps
 
 if __name__ == "__main__":
 
-    problemclass1 = GaussianHillsProblem
-    problemclass2 = problemclass1
+    problemclass_train_list = [GaussianHillsProblem]
+    problemclass_eval_list = [GaussianHillsProblem]
 
-    agent_performance = []
+    # every possible pair of problemclass 
+    problemclass_pairs = [(train, eval) for train in problemclass_train_list for eval in problemclass_eval_list]
+
+    #agent_performance = []
 
     nb_timesteps_list = [1000, 5000, 10000, 50000, 100000]
-    nb_trials = 10
+    nb_trials = 1
     
-    all_optimizer_results = []
-    all_score_matrices = []
-    agent_performance = np.zeros((nb_trials, len(nb_timesteps_list)))
+    #all_optimizer_results = []
+    #all_score_matrices = []
+    #agent_performance = np.zeros((nb_trials, len(nb_timesteps_list)))
 
-    for i, nb_timesteps in enumerate(nb_timesteps_list):
-        print(nb_timesteps)
-        aor = []
-        asm = []
-        for trial in range(nb_trials):
-            results, params_dict = train_and_eval_agent(problemclass1, problemclass2, nb_timesteps)
-            best_optimizer_count, score_matrix = agent_statistics(results, params_dict, do_plot=False)
-            print("agent performance: ", best_optimizer_count["agent"])
-            agent_performance[trial, i] = best_optimizer_count["agent"]
-            aor.append(best_optimizer_count)
-            asm.append(score_matrix)
+    for (problemclass_train, problemclass_eval) in problemclass_pairs:
+
+        for i, nb_timesteps in enumerate(nb_timesteps_list):
+            #print(nb_timesteps)
+            #aor = []
+            #asm = []
+            for trial in range(nb_trials):
+
+                run = wandb.init(reinit=True, 
+                                project="switching_optimizers", 
+                                config={"problemclass_train": problemclass_train.__name__,
+                                        "problemclass_eval": problemclass_eval.__name__,
+                                        "nb_timesteps": nb_timesteps, 
+                                        "optimization_mode" : config.policy.optimization_mode, 
+                                        "reward_system": config.environment.reward_system,
+                                        "history_len": config.model.history_len,
+                                        "lr": config.model.lr,})
+                with run:        
+
+                    results, params_dict = train_and_eval_agent(problemclass_train, problemclass_eval, nb_timesteps)
+                    best_optimizer_count, score_matrix = agent_statistics(results, params_dict, do_plot=False)
+
+                    wandb.log({"agent_count": best_optimizer_count["agent"],
+                                "all_optimizer_count": best_optimizer_count,
+                                "all_trajectories": results,
+                               "score_matrix": score_matrix,})    
             
-            # save the results
-
-            filename = "visualization/parameter_sensitivity_results/"\
-                #+ problemclass1.__name__\
-                #+ "_" + problemclass2.__name__\
-                #+ "_" + config.policy.optimization_mode 
-
-            with open(filename + 'agent_performance.pkl', 'wb') as f:
-                pickle.dump(agent_performance, f)
-            # save all_optimizer_results
-            with open(filename + 'all_optimizer_results.pkl', 'wb') as f:
-                pickle.dump(all_optimizer_results, f)
-            # save all_score_matrices
-            with open(filename + 'all_score_matrices.pkl', 'wb') as f:
-                pickle.dump(all_score_matrices, f)
-
-        all_optimizer_results.append(aor)
-        all_score_matrices.append(asm)
             
-        print("mean agent performance: ", np.mean(agent_performance[:, i]))
-    plt.plot(nb_timesteps_list, np.mean(agent_performance, axis=0))
+            """
+                print("agent performance: ", best_optimizer_count["agent"])
+                #agent_performance[trial, i] = best_optimizer_count["agent"]
+                #aor.append(best_optimizer_count)
+                #asm.append(score_matrix)
+                
+                # save the results
+
+                filename = "visualization/parameter_sensitivity_results/"\
+                    #+ problemclass1.__name__\
+                    #+ "_" + problemclass2.__name__\
+                    #+ "_" + config.policy.optimization_mode 
+
+                with open(filename + 'agent_performance.pkl', 'wb') as f:
+                    pickle.dump(agent_performance, f)
+                # save all_optimizer_results
+                with open(filename + 'all_optimizer_results.pkl', 'wb') as f:
+                    pickle.dump(all_optimizer_results, f)
+                # save all_score_matrices
+                with open(filename + 'all_score_matrices.pkl', 'wb') as f:
+                    pickle.dump(all_score_matrices, f)
+
+            all_optimizer_results.append(aor)
+            all_score_matrices.append(asm)
+                
+            print("mean agent performance: ", np.mean(agent_performance[:, i]))
+            """
+
 
     
