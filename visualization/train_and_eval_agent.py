48bdb10798cf9a0e47245dbbf7355e0d104a900d
Tidiane <camaret.tidiane@mail.com>
2022-11-29 16:01:36 +0100

    refactor

--- visualization/train_and_eval_agent.py
+++ visualization/train_and_eval_agent.py
@@ -35,7 +35,7 @@ nb_test_points = 1000
 def train_and_eval_agent(problemclass_train, problemclass_eval, agent_training_timesteps, do_plot=True):
     
     if problemclass_train == "all_except_eval":
-        problemclass_train_list = all_problems_class_list - problemclass_eval
+        problemclass_train_list = all_problems_class_list.remove(problemclass_eval)
         train_problem_list = [np.random.choice(problemclass_train_list)(x0=np.random.uniform(-xlim, xlim, size=(2))) for _ in range(nb_train_points)]
     
     elif problemclass_train == "none":
